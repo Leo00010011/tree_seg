b73a838d62e2feaaeb78303eb7fd522a995de8fe
Lauren Guerra <91337957+lauolivia@users.noreply.github.com>
2023-06-30 16:19:03 -0400

    Create city_analysis.py

---
+++ city_analysis.py
@@ -0,0 +1,57 @@
+import rasterio as rst
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def CCCscale(x):
+  return (x - np.nanpercentile(x, 2)) / (np.nanpercentile(x, 98) - np.nanpercentile(x, 2))
+
+with rst.open('/content/three_band/6010_0_0.tif') as src:
+  r = src.read(1)[:3345,:3338]
+  g = src.read(2)[:3345,:3338]
+  b = src.read(3)[:3345,:3338]
+  r_scl = CCCscale(r)
+  g_scl = CCCscale(g)
+  b_scl = CCCscale(b)
+  img =cv2.merge((r_scl,g_scl,b_scl)) 
+  plt.figure()
+  plt.imshow(img)
+
+
+
+
+import rasterio as rst
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def CCCscale(x):
+  return (x - np.nanpercentile(x, 2)) / (np.nanpercentile(x, 98) - np.nanpercentile(x, 2))
+
+def analysis_imagen(path):
+  with rst.open(path) as src:
+    r = src.read(1)[:3345,:3338]
+    g = src.read(2)[:3345,:3338]
+    b = src.read(3)[:3345,:3338]
+    r_scl = CCCscale(r)
+    g_scl = CCCscale(g)
+    b_scl = CCCscale(b)
+    # img = cv2.merge((r,g,b)) 
+    img = cv2.merge((r_scl,g_scl,b_scl)) 
+    plt.figure()
+    plt.imshow(img)
+
+def browse_all_images(path):
+  for a in range(10,180,10):
+    for b in range(4):
+      for c in range(4):
+        if a < 100:
+          analysis_imagen(f'{path}60{a}_{b}_{c}.tif')
+      else:
+        analysis_imagen(f'{path}6{a}_{b}_{c}.tif')
+
+
+
+
+browse_all_images('/content/three_band/')
+# analysis_imagen('/content/three_band/6010_0_1.tif')
