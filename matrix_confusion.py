224140a3c83a1ece8b0b9d8e912869369f285e79
Krtucho <Krtucho98@gmail.com>
2023-07-06 20:57:08 -0400

    added precision, recall and f1_score metrics

--- matrix_confusion.py
+++ matrix_confusion.py
@@ -72,3 +72,67 @@ def display_confusion_matrix_s_full(confusion_matrixs:list):
 
     confusionMatrixDisplay.plot(cmap="Blues")
     plt.show()
+
+
+# Precision
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+def calculate_precision(confusion_matrix):
+    true_negatives = confusion_matrix[0, 0]
+    true_positives = confusion_matrix[1, 1]
+    false_negatives = confusion_matrix[1, 0]
+    false_positives = confusion_matrix[0, 1]
+    
+    true_values = [true_negatives, true_positives]
+    predicted_values = [true_negatives + false_negatives, true_positives + false_positives]
+    
+    precision = precision_score(y_true=true_values, y_pred=predicted_values, zero_division=0)
+    
+    return precision
+
+
+def calculate_recall(confusion_matrix):
+    true_negatives = confusion_matrix[0, 0]
+    true_positives = confusion_matrix[1, 1]
+    false_negatives = confusion_matrix[1, 0]
+    false_positives = confusion_matrix[0, 1]
+    
+    true_values = [true_negatives, true_positives]
+    predicted_values = [true_negatives + false_negatives, true_positives + false_positives]
+    
+    recall = recall_score(y_true=true_values, y_pred=predicted_values, zero_division=0)
+    
+    return recall
+
+def calculate_f1(confusion_matrix):
+    precision = calculate_precision(confusion_matrix)
+    recall = calculate_recall(confusion_matrix)
+    
+    f1 = 2 * (precision * recall) / (precision + recall)
+    
+    return f1
+
+# Create example confusion matrix
+confusion_matrix = np.array([[80, 20], [30, 70]])
+
+# Calculate precision using the method
+precision = calculate_precision(confusion_matrix)
+
+# Print the result
+print("The precision is:", precision)
+
+# Calculate recall using the method
+recall = calculate_recall(confusion_matrix)
+
+# Print the result
+print("The recall is:", recall)
+
+# Create example confusion matrix
+confusion_matrix = np.array([[80, 20], [30, 70]])
+
+# Calculate F1 score using the method
+f1 = calculate_f1(confusion_matrix)
+
+# Print the result
+print("The F1 score is:", f1)
